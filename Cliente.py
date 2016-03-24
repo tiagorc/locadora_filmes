d3f037b374cc2a76fbebcebe2674251222bb72aa
tiagorc <tiagobrasil2005@yahoo.com.br>
2016-03-24 09:24:07 -0300

    Split method Expressao in two another methods to improve the comprehension

--- Cliente.py
+++ Cliente.py
@@ -16,10 +16,30 @@ class Cliente():
     def __init__(self, nome):
         self._nome = nome
         self._alocacoes = []
-    def addicionarAlocacao(self, arg): # art de tipo alocacao
+    def addicionarAlocacao(self, arg): # arg de tipo alocacao
         self._alocacoes.append(arg)
     def getNome(self):
         return self._nome
+
+    def calculaPontosAlocacao(self, pontos):
+        return pontos + 1
+
+    def quantidadePara(self, alocacao):
+        resultado = 0.0
+        #Determinar valores para cada linha
+        if alocacao.getFilme().getPrecoCodigo() == REGULAR:
+            resultado = resultado + 2
+            if alocacao.getDiasAlocados()>2:
+                resultado = resultado + (alocacao.getDiasAlocados()-2)*1.5
+        elif alocacao.getFilme().getPrecoCodigo() == NOVA_RELEASE:
+            resultado = resultado + 3
+        elif alocacao.getFilme().getPrecoCodigo() == CRIANCAS:
+            resultado = resultado + 1.5
+            if alocacao.getDiasAlocados()>3:
+                resultado = resultado + (alocacao.getDiasAlocados()-3)*1.5
+
+        return resultado
+
     def Expresao(self):
         totalQuantidade = 0.0
         pontosFrequenciaAlocacao = 0
@@ -31,25 +51,18 @@ class Cliente():
             estaQuantidade = 0.0
             #cada = next(alocacoes) #cada tipo alocacao
             #cada dever ser de tipo alocacao
-            #Determinar valores para cada linha
-            if cada.getFilme().getPrecoCodigo() == REGULAR:
-                estaQuantidade = estaQuantidade + 2
-                if cada.getDiasAlocados()>2:
-                    estaQuantidade = estaQuantidade + (cada.getDiasAlocados()-2)*1.5
-            elif cada.getFilme().getPrecoCodigo() == NOVA_RELEASE:
-                estaQuantidade = estaQuantidade + 3
-            elif cada.getFilme().getPrecoCodigo() == CRIANCAS:
-                estaQuantidade = estaQuantidade + 1.5
-                if cada.getDiasAlocados()>3:
-                    estaQuantidade = estaQuantidade + (cada.getDiasAlocados()-3)*1.5
+            estaQuantidade = self.quantidadePara(cada)
 
             #adicionar pontos de locador frequente
-            pontosFrequenciaAlocacao = pontosFrequenciaAlocacao + 1
+            pontosFrequenciaAlocacao = self.calculaPontosAlocacao(pontosFrequenciaAlocacao)
             #adicionar bonus para uma locação de dois dias para lançamentos
             if cada.getFilme().getPrecoCodigo() == Filme.NOVA_RELEASE and cada.getDiasAlocados()>1:
                 pontosFrequenciaAlocacao = pontosFrequenciaAlocacao +1
             #mostrar informacoes para esta locacao
             resultado = resultado + ' '+cada.getFilme().getTitulo()+' '+ str(estaQuantidade)+'\n'
+
+            #calcular totalQuantidade
+            totalQuantidade = totalQuantidade + cada.getFilme().getPrecoCodigo()
 
         #adicionar rodape do relatorio
         resultado = resultado + "Quantia devida é "+ str(totalQuantidade)+"\n"
@@ -57,10 +70,10 @@ class Cliente():
         return resultado
 
 if __name__ == '__main__':
-    meuCliente = Cliente('Ruben')
+    meuCliente = Cliente('Ruben')#init: nome
     print meuCliente.getNome()
-    fil01 = Filme('Titanic',2)
-    alo01 = Alocacao(fil01,5) #filme, dias locados
+    fil01 = Filme('Titanic',2) #init: (titulo, preco)
+    alo01 = Alocacao(fil01,5) #filme, dias locados #init: (Filme, diasAlocado)
     alo02 = Alocacao(fil01,2) #filme, dias locados
     alo03 = Alocacao(fil01,1) #filme, dias locados
 
@@ -69,3 +82,10 @@ if __name__ == '__main__':
     meuCliente.addicionarAlocacao(alo03)
 
     print meuCliente.Expresao()
+
+
+
+# problemas
+#
+# resultado quantidadetotal errada
+# 
